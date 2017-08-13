090657270f2c70faf33df74b67cf858b21e9e27a
Juan Caicedo <juan@spanishdict.com>
2017-08-13 11:34:36 +0200

    listed to keyboard events

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import React from 'react'
 const state = {
   nextKey: 'f'
 }
 
-const Index = () => (
-  <div>{state.nextKey}</div>
-)
+class Index extends React.Component {
 
+  componentDidMount() {
+    window.onkeyup = (e) => {
+      const key = e.keyCode ? e.keyCode : e.which
+      console.log("key", key)
+    }
+  }
+
+  render() {
+    return <div>{state.nextKey}</div>
+  }
+}
 export default Index
