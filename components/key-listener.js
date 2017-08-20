f6b388eff6b76dc256a88da6c9f299c47c6316f1
Juan Caicedo <juan@spanishdict.com>
2017-08-20 19:59:40 +0200

    ignore shift key

--- components/key-listener.js
+++ components/key-listener.js
@@ -4,11 +4,25 @@ import { connect } from 'react-redux'
 import keycode from 'keycode'
 import { recordKey } from '../store'
 
+const getKey = ({ which, shiftKey }) => {
+  const key = keycode(which)
+  if (key === 'shift') {
+    return null
+  }
+  if (shiftKey) {
+    return key.toUpperCase()
+  }
+
+  return key
+}
+
 class KeyListener extends React.Component {
   componentDidMount() {
     window.onkeyup = (e) => {
-      const key = keycode(e)
-      this.props.recordKey(key)
+      const key = getKey(e)
+      if (key) {
+        this.props.recordKey(key)
+      }
     }
   }
 
