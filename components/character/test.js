656b473baa09d6c4ffdc0d1dde680ba9d69953e3
Juan Caicedo <retiredcanadianpoet@gmail.com>
2017-09-02 12:31:59 +0200

    test characters

--- components/character/test.js
+++ components/character/test.js
@@ -1,7 +1,34 @@
-import { expect } from 'chai'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+import Character from './view'
 
-describe('test', () => {
-  it('should work', () => {
-    expect(false).to.be.true
+describe('view', () => {
+  it('should render current character', () => {
+    const props = {
+      idx: 1,
+      currentIndex: 1
+    }
+    const tree = renderer.create(<Character {...props} />).toJSON()
+    expect(tree).toHaveStyleRule('text-decoration', 'underline')
+  })
+
+  it('should render correct character', () => {
+    const props = {
+      wasCorrect: true,
+      idx: 2,
+      currentIndex: 1
+    }
+    const tree = renderer.create(<Character {...props} />).toJSON()
+    expect(tree).toHaveStyleRule('color', 'blue')
+  })
+
+  it('should render incorrect character', () => {
+    const props = {
+      wasCorrect: false,
+      idx: 2,
+      currentIndex: 1
+    }
+    const tree = renderer.create(<Character {...props} />).toJSON()
+    expect(tree).toHaveStyleRule('color', 'red')
   })
 })
