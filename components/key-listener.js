796d806a660160f01a017a217e5601f8a396982d
Juan Caicedo <retiredcanadianpoet@gmail.com>
2017-09-03 17:06:43 +0200

    don't listen to keys after finish

--- components/key-listener.js
+++ components/key-listener.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import { getIsFinished } from '../store/selectors'
 import { recordKey } from '../store'
 
 class KeyListener extends React.Component {
-  componentDidMount() {
-    window.onkeypress = (e = window.event) => {
-      const { key } = e
-      if (key) {
-        this.props.recordKey(key)
-      }
+  handleKeyPress({ key }) {
+    if (key) {
+      this.props.recordKey(key)
     }
+  }
+
+  componentDidMount() {
+    window.onkeypress = (e = window.event) => this.handleKeyPress(e)
+  }
+
+  componentWillUnmount() {
+    window.onkeypress = null
   }
 
   render() {
@@ -18,14 +24,8 @@ class KeyListener extends React.Component {
   }
 }
 
-const mapDispathToProps = dispatch => {
-  return {
-    recordKey: bindActionCreators(recordKey, dispatch),
-  }
-}
+const mapDispathToProps = dispatch => ({
+  recordKey: bindActionCreators(recordKey, dispatch),
+})
 
-const mapStateToProps = state => {
-  return state
-}
-
-export default connect(mapStateToProps, mapDispathToProps)(KeyListener)
+export default connect(null, mapDispathToProps)(KeyListener)
