8d4b4c3313e6b731472bfd9915def538930f9ba4
Juan Caicedo <retiredcanadianpoet@gmail.com>
2017-09-17 18:20:53 +0200

    tidy up styles

--- components/frame/styles.js
+++ components/frame/styles.js
@@ -4,5 +4,7 @@ export const Frame = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  margin: 20px;
+  margin: 20px auto;
+  min-width: 550px;
+  max-width: 720px;
 `
