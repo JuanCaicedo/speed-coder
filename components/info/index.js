d112281d4e15de8b1ea1259a93957b81ec0c847d
Juan Caicedo <retiredcanadianpoet@gmail.com>
2017-09-17 13:29:14 +0200

    add info line

---
+++ components/info/index.js
@@ -0,0 +1,12 @@
+import { InfoLine } from './styles'
+
+const Info = () => (
+  <InfoLine>
+    <span>Made with love by </span>
+    <a href="http://www.juancaicedo.com/">Juan Caicedo</a>
+    <span>— fork or suggest edits on </span>
+    <a href="https://github.com/JuanCaicedo/speed-coder">GitHub</a>
+  </InfoLine>
+)
+
+export default Info
---
+++ components/info/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const InfoLine = styled.div`
+  position: absolute;
+  font-size: 22px;
+  text-align: center;
+  width: 100%;
+  color: white;
+  bottom: 20px;
+
+  a {
+    color: white;
+  }
+`
